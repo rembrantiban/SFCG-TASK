91b326c5817996e43a8c66fd3ea928f1df5ef919
RembrantIban <rembrantiban62@gmail.com>
2025-11-19 15:29:10 +0800

    Updated Code

--- client/src/pages/userRequestpage.jsx
+++ client/src/pages/userRequestpage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { FileText } from "lucide-react";
 import StaffHeader from "../component/staffheader/header";
-import { WorkOrderRequestModal} from "../component/modal/WorkOrderRequestModal";
+import { WorkOrderRequestModal } from "../components/modal/WorkOrderRequestModal";
 import { FilePlus } from "lucide-react";
 import UserRequest from "../component/userRequest/userRequest";
 
